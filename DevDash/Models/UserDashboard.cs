c4b426d29d0e8c93674f724546159ebc2d352c29
scosacescu <stephanie.cosacescu@gmail.com>
2017-11-17 15:09:22 -0800

    Modified some Views/css, added Models, and modified Account Controller per ApplicationUser model

---
+++ DevDash/Models/UserDashboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevDash.Models
+{
+    public class UserDashboard
+    {
+        public Guid DashboardID { get; set; }
+        public Guid UserID { get; set; }
+        public string RepoID { get; set; }
+        public string BoardID { get; set; }
+        public string DashboardName { get; set; }
+    }
+}
---
+++ DevDash/Models/UserGithub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevDash.Models
+{
+    public class UserGithub
+    {
+        public Guid UserID { get; set; }
+        public string RepoID { get; set; }
+        public string RepoName { get; set; }
+        public string IssuesURL { get; set; }
+    }
+}
